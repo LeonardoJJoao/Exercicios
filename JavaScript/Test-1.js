e02f187a1438fc2e016dde06ab793bf2cc907ae7
Leonardo <leonardoguerreirojoao@gmail.com>
2019-02-27 16:07:33 +0000

    final versio-bug aparece no fim - 21-undifined

--- JavaScript/Test-1.js
+++ JavaScript/Test-1.js
@@ -29,12 +29,20 @@ console.log(getSentence()); */
 var request = new XMLHttpRequest();
 var filmTitleArray = [];
 
+var favouriteMovie = document.getElementById("favouriteMovies");
+var favouriteFilmsNumber = 0;
+var favouriteFilms = [];
+var index;
+
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', false); // adicionar ?limit=4 pra ter um limite de 4 -- Exercicio 1 da API
 
-request.onload = function () {
+request.onload = function () 
+{
     var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
+    if (request.status >= 200 && request.status < 400) 
+    {
+        data.forEach(movie => 
+        {
             filmTitleArray.push(movie.title);
             console.log(movie.title);
             console.log(filmTitleArray);
@@ -46,37 +54,57 @@ request.onload = function () {
 
 request.send();
 
-function newSentence(){
-    document.getElementById("selected-sentence").innerHTML = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];;
+document.getElementById("selected-sentence").innerHTML = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];
+
+function getIndex() 
+{
+    index = filmTitleArray.indexOf(document.getElementById("selected-sentence").innerHTML);
+    return index;
 }
 
-document.getElementById("selected-sentence").innerHTML = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];;
+function newSentence() 
+{
+    if (filmTitleArray.length <= 0) 
+    {
+        document.getElementById("selected-sentence").innerHTML = "End of list";
+    }
+    else if (favouriteFilms.includes(document.getElementById("selected-sentence").innerHTML)) 
+    {
+        getIndex()
 
-var favouriteMovie = document.getElementById("favouriteMovies");
-var favouriteFilmsNumber = 0;
-var favouriteFilms = [];
+        filmTitleArray.splice(index, 1);
+        console.log(filmTitleArray);
 
-function addSentenceTable(){
-    if(favouriteFilms.includes(document.getElementById("selected-sentence").innerHTML)){
+        document.getElementById("selected-sentence").innerHTML = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];
+        console.log(filmTitleArray.length);
+    }
+
+    document.getElementById("selected-sentence").innerHTML = filmTitleArray[Math.floor(Math.random() * filmTitleArray.length)];
+}
+
+function addSentenceTable() 
+{
+    if (favouriteFilms.includes(document.getElementById("selected-sentence").innerHTML)) 
+    {
         console.log("alredy in array");
-    }else{
+    } else{
         favouriteFilms.push(document.getElementById("selected-sentence").innerHTML);
         console.log(favouriteFilms);
 
-        favouriteFilmsNumber ++;
+        favouriteFilmsNumber++;
 
         const tr = document.createElement('tr');
 
         td_number = document.createElement('td');
-        td_number.setAttribute('id','listnumber' + favouriteFilmsNumber);
-        
+        td_number.setAttribute('id', 'listnumber' + favouriteFilmsNumber);
+
         const td_name = document.createElement('td');
-        td_name.textContent = favouriteFilms[favouriteFilms.length -1];
+        td_name.textContent = favouriteFilms[favouriteFilms.length - 1];
 
         favouriteMovie.appendChild(tr);
         tr.appendChild(td_number);
         tr.appendChild(td_name);
-        
-        document.getElementById("listnumber" + favouriteFilmsNumber).innerHTML = favouriteFilmsNumber; 
+
+        document.getElementById("listnumber" + favouriteFilmsNumber).innerHTML = favouriteFilmsNumber;
     }
 }
